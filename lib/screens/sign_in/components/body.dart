d8b086bfacd2775a4b471b082de18ba14afe20d7
Toan <dangtoan0405@gmail.com>
2020-12-28 16:30:20 +0700

    update file

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:what_eat_v2/components/no_account_text.dart';
-import 'package:what_eat_v2/constants.dart';
 
 import 'package:what_eat_v2/size_config.dart';
 
